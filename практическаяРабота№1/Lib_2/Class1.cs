f7d257a5ba1af653f87d235ec312320527a1ab23
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-10-10 17:43:03 +0300

    Add files via upload

---
+++ практическаяРабота№1/Lib_2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lib_2
+{
+    public class Lib
+    {
+        static public string RandNumsK(int firstInd, int secondInd, int k, out int sum, out int kol)
+        {
+            string numbers = "1";
+            sum = 0;
+            kol = 0;
+            int temp;
+
+            if (k > 0 || k == 0) // проверка адекватности введённого k ( k < 0)
+            {
+                return "";
+            }
+
+            Random rnd = new Random();
+
+            while (sum > k && sum != k) // работа функции 
+            {
+                temp = rnd.Next(firstInd, secondInd);
+                numbers = String.Concat(numbers, " ", temp);
+                sum -= temp;
+                kol++;
+            }
+
+            return numbers;
+        }
+    }
+}
