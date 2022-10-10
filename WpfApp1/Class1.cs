4aede1ca37e0d9d07a1f3aa820ad91d8a6637a1d
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-10-10 17:31:27 +0300

    Add files via upload

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    internal class Class1
+    {
+        /// <summary>
+        /// Метод проверяет число на чётность.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsEven(int value)
+        {
+            if (value % 2 == 0) return true;
+            else return false;            
+        }
+
+
+        
+    }
+}
