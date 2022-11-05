48a4ec03f482d493663340a4035bc644debbd487
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-11-05 15:52:24 +0300

    Add files via upload

---
+++ #25. practWork№8/IShip.cs
@@ -0,0 +1,10 @@
+﻿namespace practWork_8
+{
+    internal interface IShip
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Weigth { get; set; }
+        public string Name { get; set; }
+    }
+}
